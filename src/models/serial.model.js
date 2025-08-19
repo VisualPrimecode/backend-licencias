1469c2ce6284200d0617ef35569f10e6b5a1fa00
ClaudRO <claudiorodriguez7778@gmail.com>
2025-08-19 14:27:18 -0400

    detalle

--- src/models/serial.model.js
+++ src/models/serial.model.js
@@ -213,7 +213,7 @@ const obtenerSerialDisponible2 = async (producto_id, woocommerce_id) => {
     // 1. Seleccionar el primer serial disponible y bloquearlo
     const [rows] = await connection.query(
       `SELECT id, codigo
-       FROM serialesAux
+       FROM seriales
        WHERE producto_id = ?
          AND woocommerce_id = ?
          AND estado = 'disponible'
