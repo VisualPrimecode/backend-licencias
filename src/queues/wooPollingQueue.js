567ec342bb2fdc701cf37c4946ba7ff7c42503e6
ClaudRO <claudiorodriguez7778@gmail.com>
2025-10-08 11:21:21 -0300

    fix

--- src/queues/wooPollingQueue.js
+++ src/queues/wooPollingQueue.js
@@ -1,10 +1,17 @@
 const Queue = require('bull');
 
 const wooPollingQueue = new Queue('wooPollingQueue', {
+   redis: {
+    port: 6379,
+    host: 'tough-rat-53689.upstash.io',
+    password: 'AdG5AAIjcDFhYjRkMDViYTAzNTE0NTU0YWE4N2E4M2E3NDFjNGY1N3AxMA',
+    tls: {} // <= Requerido por Upstash para habilitar TLS
+  }
+  /*
   redis: {
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
-  }
+  }*/
 });
 
 module.exports = wooPollingQueue;
