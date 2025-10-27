248f7f1cf40401dbb5d7c1b1e727b8f61f2e3e7b
ClaudRO <claudiorodriguez7778@gmail.com>
2025-10-27 00:17:58 -0300

    fix 1

--- src/queues/StockProductoQueue.js
+++ src/queues/StockProductoQueue.js
@@ -7,9 +7,16 @@ const stockProductoProcessor = require('../workers/productoPedidoProcessor');
 
 // ⚙️ Crear la cola (misma config Redis que el resto)
 const stockProductoQueue = new Queue('stockProductoQueue', {
+  /*
   redis: {
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
+  }*/
+  redis: {
+    port: 6379,
+    host: 'tough-rat-53689.upstash.io',
+    password: 'AdG5AAIjcDFhYjRkMDViYTAzNTE0NTU0YWE4N2E4M2E3NDFjNGY1N3AxMA',
+    tls: {} // <= Requerido por Upstash para habilitar TLS
   }
 });
 
